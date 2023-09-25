61bd1bb1d9d9c67d62c669d3329a07236e65540d
Gafret Kelp <d.lemeshko@xlab-game.com>
2023-09-25 20:04:15 +0300

    test: added simple tests

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CarXCodingExercises;
+
+ListX<int> list = new ListX<int>();
+list.Add(1);
+list.Add(1);
+list.Add(1);
+list.Add(1);
+list.Add(1);
+Console.WriteLine(list);
+list.Add(5);
+Console.WriteLine(list);
+list.Insert(1, 3);
+Console.WriteLine(list);
+list.Capacity = 0;
+Console.WriteLine(list);
+list.Add(1);
+list.Add(1);
+list.Add(1);
+list.Remove(3);
+Console.WriteLine(list);
+list.RemoveAt(0);
+Console.WriteLine(list);
